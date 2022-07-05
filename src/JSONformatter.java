9ea03d5c2883c2c258a67e8d65551054b0cf5608
oleg <olegpaska.2005@gmail.com>
2022-07-05 10:17:06 +0100

    did work this time

---
+++ src/JSONformatter.java
@@ -0,0 +1,11 @@
+
+
+public class JSONparser {
+    APIhandler api = new APIhandler();
+
+    public int[] getBidAsk(String Stock){
+        String JSON = api.getQuote(Stock);
+        //TODO: make json parser
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args){
+        APIhandler API = new APIhandler();
+        System.out.println(API.getQuote("HSBA.L"));
+
+
+
+    }
+}
